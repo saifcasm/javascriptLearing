6654200d075fe0deb8096fb57b851aaa51355553
saifcasm <158443305+saifcasm@users.noreply.github.com>
2024-06-14 01:53:38 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "saifa1209@gmail"
+var accountPassword = 1234
+accountCity = "Hyderabad"
+let accountState
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,6 @@
+let name = 'Saifcasm'
+let age = 27
+let isLoggedIn = true
+let state;
+
+console.table([name, age, isLoggedIn, state])
