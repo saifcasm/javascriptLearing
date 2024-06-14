4d8a715feb73db9c3b12425d50412c1b84c1312b
saifcasm <158443305+saifcasm@users.noreply.github.com>
2024-06-14 02:02:48 +0000

    conversation of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,10 @@
+let score = '33bac'
+
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//in boolean 1-true, 0-false, ""-false, "Saifcasm"-true
