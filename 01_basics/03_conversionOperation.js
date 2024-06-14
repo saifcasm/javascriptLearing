4b04747c481f69a01d54b34a7e889e8bad43016e
saifcasm <158443305+saifcasm@users.noreply.github.com>
2024-06-14 02:14:00 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -8,3 +8,17 @@ console.log(typeof valueInNumber);
 console.log(valueInNumber);
 
 //in boolean 1-true, 0-false, ""-false, "Saifcasm"-true
+
+//**************** Operations************************* */
+
+console.log(2+'1');
+
+console.log('2'+1);
+
+console.log(1+2+'2');
+
+console.log(+true); //1 
+
+//console.log(true+); //this will throw an error
+
+console.log(+"") // 0
