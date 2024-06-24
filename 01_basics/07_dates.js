e673505e4d9dcf040a4e78977f9c220ca9f37185
saifcasm <158443305+saifcasm@users.noreply.github.com>
2024-06-24 01:20:51 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,10 @@
+let myDate = new Date()
+
+console.log(myDate)
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toISOString())
+console.log(myDate.toLocaleString())
+console.log(myDate.toTimeString())
+console.log(myDate.toLocaleTimeString())
+console.log(myDate.toUTCString())
